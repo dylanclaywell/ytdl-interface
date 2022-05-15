993060a1be24c940424a302977f53931a9bd511e
Dylan Claywell <djclaywell@gmail.com>
2022-05-15 12:37:48 -0500

    Grab the filesize

--- pages/api/getQueueStatus.ts
+++ pages/api/getQueueStatus.ts
@@ -1,10 +1,59 @@
 import { NextApiHandler } from 'next'
+import { exec } from 'child_process'
+import path from 'path'
 
 import { getDatabase } from '../../lib/database'
 import logger from '../../lib/logger'
-import { QueuedVideo } from '../../types/getQueueStatus'
+import {
+  GetQueueStatusResponse,
+  DBQueuedVideo,
+  QueuedVideo,
+} from '../../types/getQueueStatus'
 
-const handler: NextApiHandler = async (req, res) => {
+async function getQueuedVideos() {
+  const database = await getDatabase()
+  return await new Promise<DBQueuedVideo[]>((resolve) => {
+    database.all(
+      `select uuid, filename, extension, status from videos`,
+      (error, rows: DBQueuedVideo[]) => {
+        if (error) {
+          resolve([])
+        }
+
+        resolve(rows)
+      }
+    )
+  })
+}
+
+async function getVideoFilesize(
+  uuid: string,
+  filename: string,
+  extension: string
+) {
+  return await new Promise<string>((resolve) => {
+    exec(
+      `ls -lh "${path.resolve(
+        __dirname,
+        `../../../../output/${filename}.${extension}`
+      )}"*`,
+      (error, stdout) => {
+        if (error) {
+          logger.log(
+            'error',
+            `Could not get get filesize for video ${uuid}: ${error.message}`
+          )
+        }
+
+        const splitOutput = stdout.split(' ')
+
+        resolve(splitOutput[4])
+      }
+    )
+  })
+}
+
+const handler: NextApiHandler<GetQueueStatusResponse> = async (req, res) => {
   logger.log(
     'debug',
     `Request: /api/getQueueStatus args: ${JSON.stringify(req.body, null, 2)}`
@@ -20,18 +69,23 @@ const handler: NextApiHandler = async (req, res) => {
 
   const database = await getDatabase()
 
-  const videos = await new Promise<QueuedVideo[]>((resolve) => {
-    database.all(
-      `select uuid, status from videos`,
-      (error, rows: QueuedVideo[]) => {
-        if (error) {
-          resolve([])
-        }
+  const queuedVideos = await getQueuedVideos()
 
-        resolve(rows)
-      }
+  const videos: QueuedVideo[] = []
+
+  for (const video of queuedVideos) {
+    const filesize = await getVideoFilesize(
+      video.uuid,
+      video.filename,
+      video.extension
     )
-  })
+
+    videos.push({
+      uuid: video.uuid,
+      status: video.status,
+      filesize,
+    })
+  }
 
   const response = { videos }
 
--- types/getQueueStatus.ts
+++ types/getQueueStatus.ts
@@ -1,10 +1,24 @@
 import { QueuedVideoStatus } from './queueVideos'
 
+export interface DBQueuedVideo {
+  uuid: string
+  status: QueuedVideoStatus
+  filename: string
+  extension: string
+}
+
 export interface QueuedVideo {
   uuid: string
   status: QueuedVideoStatus
+  filesize: string
 }
 
-export interface GetQueueStatusResponse {
+interface SuccessResponse {
   videos: QueuedVideo[]
 }
+
+interface ErrorResponse {
+  message: string
+}
+
+export type GetQueueStatusResponse = SuccessResponse | ErrorResponse
